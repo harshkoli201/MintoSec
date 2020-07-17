069b5ae349f7e8e06a0be4f3dcc3f670d34286ec
Harsh Koli <55285590+harshkoli201@users.noreply.github.com>
2020-07-17 16:39:29 +0530

    Add files via upload

---
+++ mintosec.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class Hell
+{
+	public static void main(String args[])
+	{
+		int b,i,a;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Mins to convert it into Secs");
+		a=sc.nextInt();
+		b=a*60;
+		System.out.println("Your Secs is " +b);
+	}
+}
+	
+
